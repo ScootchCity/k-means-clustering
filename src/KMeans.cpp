41c1060ad85556d9255f5568b45d0552e4aa09b3
Lucy <Lucy@BOX-1>
2025-11-27 16:57:16 -0800

    made kmeans header and skeleton for the cpp

--- src/KMeans.cpp
+++ src/KMeans.cpp
@@ -1,3 +1,39 @@
 #include "KMeans.h"
 
-// Implementation file for KMeans algorithm
+//public
+KMeans::KMeans(vector<Point> points, int k, double convergence_threshold){
+    
+}
+
+void KMeans::initialize(){
+
+}
+
+void KMeans::step(){
+
+}
+
+bool KMeans::hasConverged(){
+    
+}
+
+vector<Point> KMeans::getPoints(){
+
+}
+
+vector<Cluster> KMeans::getClusters(){
+
+}
+
+//private
+void KMeans::assignPointsToClusters(){
+
+}
+
+void KMeans::updateClusterCenters(){
+
+}
+
+double KMeans::calculateDistance(Point pt, Cluster cl){
+
+}
--- src/KMeans.h
+++ src/KMeans.h
@@ -2,6 +2,7 @@
 #define KMEANS_H
 
 #include <vector>
+#include <string>
 #include "Point.h"
 #include "Cluster.h"
 
@@ -9,13 +10,24 @@ using namespace std;
 
 class KMeans {
 public:
-    //l-means algorithm implementation will go here
+    KMeans(vector<Point> points, int k, double convergence_threshold);
+
+    void initialize();              //initilize k centers
+    void step();                    //step the algo
+    bool hasConverged();            //see if convergence is below threshold
+    vector<Point> getPoints();      //get the current vector of points (with assigned clusters)
+    vector<Cluster> getClusters();  //get the current clusters
 
 private:
-    vector<Point> points;
-    vector<Cluster> clusters;
-    int k;
-    double epsilon;
+    vector<Point> points;               //all the points
+    vector<Cluster> clusters;           //all the clusters
+    vector<Cluster> prev_clusters;      //previous clusters to check convergence
+    int k;                              //number of clusters to make
+    double convergence_threshold;      //value of convergence at which algo is done
+
+    void assignPointsToClusters();      //assign cluster_id of point to nearest cluster
+    void updateClusterCenters();        //recalculate cluster centers
+    double calculateDistance(Point pt, Cluster cl); //eucledian distance between a point and a cluster
 };
 
 #endif // KMEANS_H
