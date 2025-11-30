7c81c384c3b03c3eb20738fcbb081a630095db96
aidan01smith <aidan.smith01@mymail.champlain.edu>
2025-11-29 19:15:18 -0500

    by god I have made them plot

--- plot_data.py
+++ plot_data.py
@@ -13,32 +13,34 @@ def read_data(csv_file):
 # oooh pretty colors
 
 def plot_colors(data):
-    colors = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', '#98D8C8', 
-              '#F7DC6F', '#BB8FCE', '#85C1E2', '#F8B739', '#52BE80']
+    colors = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', '#98D8C8', '#F7DC6F', '#BB8FCE', '#85C1E2', '#F8B739', '#52BE80']
     
-    plt.figure(figsize=(10, 8))
+    # use explicit Figure and Axes so we can return the Figure object
+    fig, ax = plt.subplots(figsize=(10, 8))
 
-    for i, column in enumerate(data.columns[1:]):
-        
+    i = 0
+    for column in data.columns[1:]:
         # plot only points (no connecting lines)
-        
-        plt.scatter(
+        ax.scatter(
             data[data.columns[0]],
             data[column],
             color=colors[i % len(colors)],
             s=50,
             label=column,
         )
+        i = i + 1
+
+    ax.set_xlabel(data.columns[0])
+    ax.set_ylabel('Values')
     
     
-    plt.xlabel(data.columns[0])
-    plt.ylabel('Values')
-    plt.title('Data Plots')
-    plt.legend()
-    plt.grid(True)
+    ax.set_title('Data Plots')
+    ax.legend()
+    
+    
+    ax.grid(True)
 
-    return plt.gcf()
-
+    return fig
 
 
 def save_plot(figure, filename='cluster_plot.png'):    
@@ -54,14 +56,9 @@ def visualize_data(csv_file, output_file='data_plot.png'):
 
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-
-        sys.exit(1)
-
-    csv_file = sys.argv[1]
-    data = read_data(csv_file)
-    plot_figure = plot_colors(data)
+csv_file = sys.argv[1]
+data = read_data(csv_file)
+plot_figure = plot_colors(data)
     
-    # plt.show()
-    save_plot(plot_figure, 'data_plot.png')    
+# plt.show()
+save_plot(plot_figure, 'data_plot.png')    
